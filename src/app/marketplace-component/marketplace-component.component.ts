5f344b00379493f1158bb89016ad14542fc8b8ea
LostIn5auce02 <srikar.turaga@ku.edu>
2023-04-18 21:20:43 -0500

    Added MarketPlace Functionality

--- src/app/marketplace-component/marketplace-component.component.ts
+++ src/app/marketplace-component/marketplace-component.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, ViewContainerRef } from '@angular/core';
 import { DialogComponentComponent } from '../dialog-component/dialog-component.component'
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { HttpClient } from '@angular/common/http';
 import { GlobalComponent } from '../global/global.component';
+import { HomeComponentComponent } from '../home-component/home-component.component';
 @Component({
   selector: 'app-marketplace-component',
   templateUrl: './marketplace-component.component.html',
@@ -12,7 +13,7 @@ export class MarketplaceComponentComponent {
 
   dataSource: any;
   displayedColumns: string[] = ['CID','name','formatcommander','type','colors','manacost','maintype','set','rarity','price'];
-  constructor(private http: HttpClient, public dialog: MatDialog)
+  constructor(private http: HttpClient, public dialog: MatDialog, private view: ViewContainerRef)
   {
     const obj = {"email": GlobalComponent.username};
     this.http.get<any>("http://localhost:5191/getCards")
@@ -23,15 +24,50 @@ export class MarketplaceComponentComponent {
       }
       else
       {
-        alert("ERROR! No Cards For User");
+        alert("ERROR! No Cards");
       }
   }) 
   }
 
   rowClick(row: any)
   {
-    alert(row["name"]);
-    //this.openDialog(row);
+    var choice = prompt("Do you want to buy? (Y/N)")?.toString();
+    console.log(typeof(choice));
+    if(choice == "Y")
+    {
+      console.log("HERE");
+      const card = {
+        "email": GlobalComponent.username,
+        "CID": row['CID']
+      };
+      GlobalComponent.buy.push(card);
+    }
+  }
+
+  buyCards()
+  {
+    if(GlobalComponent.buy.length == 0)
+    {
+      alert("No Cards To Buy.");
+    }
+    else
+    {
+      this.http.post<any>("http://localhost:5191/buyCards", GlobalComponent.buy)
+      .subscribe(res=>{
+        if(res != 0)
+        {
+          alert("Cards Successfully Bought.")
+          GlobalComponent.buy=[];
+          const injector = this.view.parentInjector;
+          const parent: HomeComponentComponent = injector.get<HomeComponentComponent>(HomeComponentComponent);
+          parent.userBalance = res;
+        }
+        else
+        { 
+          alert("Error! Cards Cannot Be Sold.");
+        }
+      })
+    }
   }
 
 }
